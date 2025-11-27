407876b730f49f884446d407bda8f0de1db87295
Ancava2000 <anacarrovaz@gmail.com>
2025-11-27 05:32:11 -0500

    std::string

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -6,19 +6,18 @@
 /*   By: acarro-v <acarro-v@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 13:13:37 by acarro-v          #+#    #+#             */
-/*   Updated: 2025/11/01 14:30:30 by acarro-v         ###   ########.fr       */
+/*   Updated: 2025/11/27 05:31:47 by acarro-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
 
-using namespace std;
 
 int main(void){
-	string brain = "HI THIS IS BRAIN";
-	string* stringPTR = &brain;
-	string& stringREF = brain;
+	std::string brain = "HI THIS IS BRAIN";
+	std::string* stringPTR = &brain;
+	std::string& stringREF = brain;
 
 	std::cout << "brain memory address: " << &brain << std::endl;
 	std::cout << "stringPTR memory address: " << stringPTR << std::endl;
